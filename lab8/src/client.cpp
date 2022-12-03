b92be38da42a7eb038c3e43a2ea71ebbd135ac5b
lolainta <lolainta@gmail.com>
2022-12-03 20:17:32 +0800

    feat: wait and stop udp (failed when submit)

--- lab8/src/client.cpp
+++ lab8/src/client.cpp
@@ -14,6 +14,17 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include <net/if.h>
+#include <sysexits.h>
+#include <ifaddrs.h>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
 #define err_quit(m) { perror(m); exit(-1); }
 
 #define NIPQUAD(s)    ((unsigned char *) &s)[0], \
@@ -21,82 +32,138 @@
                     ((unsigned char *) &s)[2], \
                     ((unsigned char *) &s)[3]
 
-static int s = -1;
+#define cout cout<<"[Client] "
+
+static int sock=-1;
 static struct sockaddr_in sin;
-static unsigned seq;
-static unsigned count = 0;
+static uint16_t seq=0;
+static unsigned timeout_t=0;
 
-typedef struct {
-    unsigned seq;
-    struct timeval tv;
-}    ping_t;
+const unsigned int MAX_SIZE=32768;
 
-double tv2ms(struct timeval *tv) {
-    return 1000.0*tv->tv_sec + 0.001*tv->tv_usec;
-}
+typedef struct{
+    uint16_t seq;
+    uint16_t checksum;
+}__attribute__((packed))request;
 
-void do_send(int sig) {
-    unsigned char buf[1024];
-    if(sig == SIGALRM) {
-        ping_t *p = (ping_t*) buf;
-        p->seq = seq++;
-        gettimeofday(&p->tv, NULL);
-        if(sendto(s, p, sizeof(*p)+16, 0, (struct sockaddr*) &sin, sizeof(sin)) < 0)
+typedef struct response{
+    uint16_t seq;
+    uint16_t size;
+    uint32_t checksum;
+    uint8_t filename[16];
+    uint8_t file[MAX_SIZE];
+    response(uint16_t _seq,uint16_t _size,uint8_t*_file,string _fname):seq(_seq),size(_size){
+        strncpy((char*)file,(char*)_file,size);
+        strcpy((char*)filename,_fname.c_str());
+        checksum=0;
+    }
+}__attribute__((packed))response;
+
+vector<response> files;
+void do_send(int sig){
+    if(sig==SIGALRM){
+        cout<<"Send file: "<<seq<<endl;
+        if(sendto(sock,files.data()+seq,sizeof(response)+16,0,(sockaddr*)&sin,sizeof(sin))<0)
             perror("sendto");
         alarm(1);
     }
-    count++;
-    if(count > 10) exit(0);
+    timeout_t++;
+    cout<<"timeout_t: "<<timeout_t<<endl;
+    if(timeout_t>10)
+        exit(0);
 }
 
-int main(int argc, char *argv[]) {
-    if(argc < 3) {
-        return -fprintf(stderr, "usage: %s ... <port> <ip>\n", argv[0]);
+char ip[32];
+void get_ip(){
+    ifaddrs*ifs=nullptr;
+    getifaddrs(&ifs);
+    for(auto ifen=ifs;ifen!=nullptr;ifen=ifen->ifa_next){
+        sa_family_t addr_fam=ifen->ifa_addr->sa_family;
+        if(addr_fam==AF_INET and ifen->ifa_addr!=nullptr){
+            inet_ntop(addr_fam,&((sockaddr_in*)ifen->ifa_addr)->sin_addr,ip,32);
+            if(string(ip).substr(0,4)=="172."){
+                cout<<"IP: "<<ip<<endl;
+                break;
+            }
+        }
     }
+}
+int main(int argc,char*argv[]){
+    cout<<"Client start running."<<endl<<flush;
+    cout<<argv[0]<<' '<<argv[1]<<' '<<argv[2]<<' '<<argv[3]<<' '<<argv[4]<<endl;
+    if(argc<3)
+        return -fprintf(stderr,"usage: %s ... <port> <ip>\n",argv[0]);
+    assert(argc==5);
 
-    srand(time(0) ^ getpid());
+    get_ip();
 
-    setvbuf(stdin, NULL, _IONBF, 0);
-    setvbuf(stderr, NULL, _IONBF, 0);
-    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stdin,NULL,_IONBF,0);
+    setvbuf(stderr,NULL,_IONBF,0);
+    setvbuf(stdout,NULL,_IONBF,0);
 
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_port = htons(strtol(argv[argc-2], NULL, 0));
-    if(inet_pton(AF_INET, argv[argc-1], &sin.sin_addr) != 1) {
-        return -fprintf(stderr, "** cannot convert IPv4 address for %s\n", argv[1]);
-    }
+    cout<<sizeof(request)<<' '<<sizeof(response)<<endl;
 
-    if((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+    memset(&sin,0,sizeof(sin));
+    sin.sin_family=AF_INET;
+    sin.sin_port=htons(strtol(argv[3],NULL,0));
+//    if(inet_pton(AF_INET,argv[4],&sin.sin_addr)!=1)
+    if(inet_pton(AF_INET,ip,&sin.sin_addr)!=1)
+        return -fprintf(stderr,"** cannot convert IPv4 address for %s\n",argv[argc-1]);
+//    sin.sin_addr.s_addr=inet_addr(ip);
+
+    if((sock=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP))<0)
         err_quit("socket");
+    printf("[Client] %u.%u.%u.%u/%u\n",NIPQUAD(sin.sin_addr), ntohs(sin.sin_port));
 
-    signal(SIGALRM, do_send);
+    string dir(argv[1]);
+    uint8_t buffer[MAX_SIZE];
+    uint16_t cnt=0;
+    for(const auto&file:filesystem::directory_iterator(dir)){
+        memset(buffer,0,sizeof(buffer));
+        ifstream in(file.path(),ios::in|ios::binary);
+        unsigned int sz=file_size(file.path());
+        assert(sz<MAX_SIZE);
+        if(in.fail())
+            perror("ifstream");
+        in.read((char*)buffer,sz);
+        files.emplace_back(cnt++,sz,buffer,file.path().filename());
+        in.close();
+    }
+    cout<<"File stored"<<endl;
+    assert(stoi(argv[2])==(int)files.size());
+    for(int i=0;i<stoi(argv[2]);++i)
+        assert(files[i].seq==i);
 
-    seq = rand() % 0xffffff;
-    fprintf(stderr, "PING %u.%u.%u.%u/%u, init seq = %d\n",
-        NIPQUAD(sin.sin_addr), ntohs(sin.sin_port), seq);
+    char tmp[10]="abc123\n";
+    response syn(9999,8,(uint8_t*)tmp,"SYN");
+    for(int i=0;i<5;++i)
+        sendto(sock,&syn,sizeof(response),0,(sockaddr*)&sin,sizeof(sin));
 
+    signal(SIGALRM,do_send);
     do_send(SIGALRM);
 
-    while(1) {
+    while(1){
         int rlen;
         struct sockaddr_in csin;
-        socklen_t csinlen = sizeof(csin);
-        char buf[2048];
-        struct timeval tv;
-        ping_t *p = (ping_t *) buf;
-        
-        if((rlen = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr*) &csin, &csinlen)) < 0) {
+        socklen_t csinlen=sizeof(csin);
+        request req;
+        if((rlen=recvfrom(sock,&req,sizeof(req),0,(sockaddr*)&csin,&csinlen))<0){
             perror("recvfrom");
             continue;
         }
+        timeout_t=0;
+        if(req.seq!=req.checksum){
+            cout<<"Checksum mismatch!\n"<<endl;
+            continue;
+        }
+        cout<<"ACK: "<<req.seq<<endl;
+        if(req.seq==stoi(argv[2])){
+            cout<<"Last ack got! finished\n";
+            exit(0);
+        }
 
-        gettimeofday(&tv, NULL);
-        printf("%lu.%06lu %d bytes from %u.%u.%u.%u/%u: seq=%u, time=%.6f ms\n",
-                p->tv.tv_sec, p->tv.tv_usec, rlen,
-                NIPQUAD(csin.sin_addr), ntohs(csin.sin_port),
-                p->seq, tv2ms(&tv)-tv2ms(&p->tv));
+        seq=max(seq,req.seq);
+        sendto(sock,&files[seq],sizeof(response),0,(sockaddr*)&csin,sizeof(csin));
     }
-
-    close(s);
+    close(sock);
 }
--- lab8/src/server.cpp
+++ lab8/src/server.cpp
@@ -10,43 +10,129 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <ifaddrs.h>
+#include <signal.h>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <cassert>
+
+using namespace std;
+
+
 #define err_quit(m) { perror(m); exit(-1); }
+#define cout cout<<"[Server] "
 
-int main(int argc, char *argv[]) {
-	int s;
-	struct sockaddr_in sin;
+const unsigned int MAX_SIZE=32768;
 
-	if(argc < 2) {
-		return -fprintf(stderr, "usage: %s ... <port>\n", argv[0]);
-	}
+typedef struct request{
+    uint16_t seq;
+    uint16_t checksum;
+    request(int _seq):seq(_seq),checksum(_seq){};
+}__attribute__((packed))request;
 
-	setvbuf(stdin, NULL, _IONBF, 0);
-	setvbuf(stderr, NULL, _IONBF, 0);
-	setvbuf(stdout, NULL, _IONBF, 0);
+typedef struct{
+    uint16_t seq;
+    uint16_t size;
+    uint32_t checksum;
+    uint8_t filename[16];
+    uint8_t file[MAX_SIZE];
+}__attribute__((packed))response;
 
-	memset(&sin, 0, sizeof(sin));
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(strtol(argv[argc-1], NULL, 0));
+/*
+void send_req(int sig){
+    if(sig==SIGALRM){
+        return;
+        request req(seq);
+        if(sendto(sock,&req,sizeof(req),0,(sockaddr*)&csin,sizeof(siin))<0)
+            perror("sendto");
+        alarm(1);
+    }
+}
+*/
+char ip[32];
+void get_ip(){
+    ifaddrs*ifs=nullptr;
+    getifaddrs(&ifs);
+    for(auto ifen=ifs;ifen!=nullptr;ifen=ifen->ifa_next){
+        sa_family_t addr_fam=ifen->ifa_addr->sa_family;
+        if(addr_fam==AF_INET and ifen->ifa_addr!=nullptr){
+            inet_ntop(addr_fam,&((sockaddr_in*)ifen->ifa_addr)->sin_addr,ip,32);
+            if(string(ip).substr(0,4)=="172."){
+                cout<<"IP: "<<ip<<endl;
+                break;
+            }
+        }
+    }
+}
 
-	if((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-		err_quit("socket");
+int main(int argc,char*argv[]){
+    cout<<"Server start running."<<endl;
+    cout<<argv[0]<<' '<<argv[1]<<' '<<argv[2]<<' '<<argv[3]<<endl;
+    if(argc<2)
+        return -fprintf(stderr,"usage: %s ... <port>\n",argv[0]);
+    assert(argc==4);
 
-	if(bind(s, (struct sockaddr*) &sin, sizeof(sin)) < 0)
-		err_quit("bind");
+    get_ip();
 
-	while(1) {
-		struct sockaddr_in csin;
-		socklen_t csinlen = sizeof(csin);
-		char buf[2048];
-		int rlen;
-		
-		if((rlen = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr*) &csin, &csinlen)) < 0) {
-			perror("recvfrom");
-			break;
-		}
+    int sock;
+    struct sockaddr_in sin;
 
-		sendto(s, buf, rlen, 0, (struct sockaddr*) &csin, sizeof(csin));
-	}
+    setvbuf(stdin,NULL,_IONBF,0);
+    setvbuf(stderr,NULL,_IONBF,0);
+    setvbuf(stdout,NULL,_IONBF,0);
 
-	close(s);
+    cout<<sizeof(request)<<' '<<sizeof(response)<<endl;
+
+    memset(&sin,0,sizeof(sin));
+    sin.sin_family=AF_INET;
+    sin.sin_addr.s_addr=htonl(INADDR_ANY);
+    sin.sin_port=htons(strtol(argv[3],NULL,0));
+
+    if((sock=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP))<0)
+        err_quit("socket");
+
+    if(bind(sock,(sockaddr*)&sin,sizeof(sin))<0)
+        err_quit("bind");
+
+    string dir(argv[1]);
+    std::filesystem::create_directories(dir);
+
+//    signal(SIGALRM,send_req);
+//    send_req(SIGALRM);
+
+    int seq=0;
+    response resp;
+    for(int i=0;i<stoi(argv[2]);++i){
+        sockaddr_in csin;
+        socklen_t csinlen=sizeof(csin);
+        int rlen;
+        
+//        memset(&resp,0,sizeof(resp));
+        cout<<"Waiting for "<<seq<<endl;
+        if((rlen=recvfrom(sock,&resp,sizeof(response),0,(sockaddr*)&csin,&csinlen))<0){
+            perror("recvfrom");
+            cout<<rlen<<endl;
+            break;
+        }
+        cout<<"Got: "<<resp.seq<<' '<<resp.filename<<endl;
+        if(resp.seq==9999){
+            --i;
+            continue;
+        }
+        
+        ofstream out(dir+'/'+string((char*)resp.filename),ios::out|ios::binary);
+        out.write((char*)resp.file,resp.size);
+        out.close();
+        request req(++seq);
+        sendto(sock,&req,sizeof(request),0,(sockaddr*)&csin,sizeof(csin));
+        cout<<"ACK: "<<seq<<endl;
+    }
+    cout<<"Finished\n";
+
+    close(sock);
 }
